f359933894cb41ad0e15cee504c26a1c5629c60a
Dan <nerdy.usa@gmail.com>
2018-11-29 20:45:34 +0300

    Add files via upload

--- lamp_install.sh
+++ lamp_install.sh
@@ -8,7 +8,7 @@ sudo apt-get -y update
 #sudo apt-get -y upgrade
 echo "Установка компонентов LAMP-сервера"
 sleep 1
-sudo apt-get install mc htop apache2 libapache2-mod-fastcgi php7.0 php7.0-fpm phpmyadmin mysql-client-5.7 mysql-server && echo "Выполнено"
+sudo apt-get install mc htop apache2 php7.0 php7.0-fpm phpmyadmin mysql-client-5.7 mysql-server && echo "Выполнено"
 echo "Настройка Apache2"
 sleep 1
 a2dismod npm_event
