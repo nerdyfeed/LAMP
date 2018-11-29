58fa38f68b86acff5edae5e26c199379d8c559ca
Dan <nerdy.usa@gmail.com>
2018-11-30 00:42:18 +0300

    Add files via upload

--- lamp_install.sh
+++ lamp_install.sh
@@ -17,7 +17,6 @@ echo "Установка компонентов LAMP-сервера"
 sleep 1
 dpkg --add-architecture i386
 export DEBIAN_FRONTEND=noninteractive;apt-get --allow-unauthenticated -y -q install $INSTALLPACK && echo "Выполнено"
-ln -s /usr/share/phpmyadmin /var/www/$siteName/public_html/phpmyadmin
 dlinapass=10
 rootmysqlpass=`base64 -w 10 /dev/urandom | head -n 1`
 echo "Настройка Apache2"
@@ -29,10 +28,11 @@ a2enmod proxy_fcgi
 service apache2 restart && echo "Выполнено"
 cd /etc/apache2/sites-available/
 cp 000-default.conf $siteName.conf
-echo "<VirtualHost *:80> \n ServerName $siteName \n ServerAlias www.$siteName \n ServerAdmin $siteMail \n DocumentRoot /var/www/$siteName/public_html \n ErrorLog ${APACHE_LOG_DIR}/error-$siteName.log \n CustomLog ${APACHE_LOG_DIR}/access-$siteName.log combined \n ProxyPassMatch ^/(.*\.php(/.*)?)$ unix:/run/php/php7.0-fpm.sock|fcgi://localhost/var/www/$siteName/public_html \n <IfModule mod_header.c> \n Header always append X-Frame-Options SAMEORIGIN \n Header set X-Content-Type-Options nosniff \n Header set X-XSS-Protection ""1; mode-block"" \n </iFmodule> \n <Directory /var/www/$siteName/public_html> \n AllowOverride none \n Options +Indexes +ExecCGI \n Order deny,allow \n Allow from all \n </Directory> \n</VirtualHost>" > /etc/apache2/sites-available/$siteName.conf
+echo -e "<VirtualHost *:80> \n ServerName $siteName \n ServerAlias www.$siteName \n ServerAdmin $siteMail \n DocumentRoot /var/www/$siteName/public_html \n ErrorLog ${APACHE_LOG_DIR}/error-$siteName.log \n CustomLog ${APACHE_LOG_DIR}/access-$siteName.log combined \n ProxyPassMatch ^/(.*\.php(/.*)?)$ unix:/run/php/php7.0-fpm.sock|fcgi://localhost/var/www/$siteName/public_html \n <IfModule mod_header.c> \n Header always append X-Frame-Options SAMEORIGIN \n Header set X-Content-Type-Options nosniff \n Header set X-XSS-Protection ""1; mode-block"" \n </iFmodule> \n <Directory /var/www/$siteName/public_html> \n AllowOverride none \n Options +Indexes +ExecCGI \n Order deny,allow \n Allow from all \n </Directory> \n</VirtualHost>" > /etc/apache2/sites-available/$siteName.conf
 mkdir /var/www/$siteName
 mkdir /var/www/$siteName/public_html
 touch /var/www/$siteName/public_html/index.html
+ln -s /usr/share/phpmyadmin /var/www/$siteName/public_html/phpmyadmin
 a2ensite $siteName.conf
 service apache2 restart
 a2dissite 000-default.conf
